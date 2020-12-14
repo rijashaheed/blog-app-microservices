02f33c0479851464f82eb6c31f74ac38d50960c5
rijashaheed <56384906+rijashaheed@users.noreply.github.com>
2020-12-14 20:36:21 +0500

    display list of data and integrate with microservices

--- posts/index.js
+++ posts/index.js
@@ -1,22 +1,24 @@
 const express = require("express");
 const { randomBytes } = require("crypto");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors()); // using it with express app as a middleware
 
 const posts = {};
 
 app.get("/posts", (req, res) => {
-	res.send(posts);    //request and respond both have body and status code
-											//200 201 are successful therefore there must me body but in status code 400 etc request is not succesful so we would see the error there will be no body
+	res.send(posts); //request and respond both have body and status code
+	//200 201 are successful therefore there must me body but in status code 400 etc request is not succesful so we would see the error there will be no body
 });
 
 app.post("/posts", (req, res) => {
 	const id = randomBytes(4).toString("hex");
 	console.log("indicator");
 	console.log(req);
-	const { title } = req.body; //normally request headers are read too 
+	const { title } = req.body; //normally request headers are read too
 
 	posts[id] = {
 		id,
