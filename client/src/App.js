26ec7b1cfe85cfafb5aa15cdce07df5c6972d533
Rija Shaheed <56384906+rijashaheed@users.noreply.github.com>
2020-12-16 20:39:12 +0500

    Added front-end component files

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import CreatePost from "./CreatePost";
+import PostList from "./PostList";
+
+function App() {
+	return (
+		<div className="container">
+			<CreatePost />
+			<hr />
+			<PostList />
+		</div>
+	);
+}
+
+export default App;
